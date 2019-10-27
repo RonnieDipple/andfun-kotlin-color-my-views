0d2f32c083a0db20b2d8e99abef45bd262a6e691
ronniedipple <ronniedipple@gmail.com>
2019-10-27 14:18:02 -0600

    Lesson 2 fully completed and merged into master via android studio

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-
+//Lesson 2 completed
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
